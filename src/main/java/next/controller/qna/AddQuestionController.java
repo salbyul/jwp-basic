4d34f1ede910b2fbe485a23d2f697fccb3e3298d
salbyul <kpast96@gmail.com>
2023-08-21 20:32:02 +0900

    feat: 자체 점검 요구사항 3번 (질문하기 구현)

---
+++ src/main/java/next/controller/qna/AddQuestionController.java
@@ -0,0 +1,27 @@
+package next.controller.qna;
+
+import core.mvc.AbstractController;
+import core.mvc.ModelAndView;
+import next.dao.QuestionDao;
+import next.model.Question;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AddQuestionController extends AbstractController {
+
+    private static final Logger log = LoggerFactory.getLogger(AddQuestionController.class);
+    private final QuestionDao questionDao = new QuestionDao();
+
+    @Override
+    public ModelAndView execute(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
+        request.setCharacterEncoding("UTF-8");
+        Question question = new Question(request.getParameter("writer"), request.getParameter("title"), request.getParameter("contents"));
+
+        questionDao.insert(question);
+        log.debug("Question Created!");
+        return jspView("redirect:/");
+    }
+}
