a5bfb116a870d2dd0e67d0b8661bca5d156f7b7b
salbyul <kpast96@gmail.com>
2023-08-22 18:51:35 +0900

    test: 자바 리플렉션 API 활용해 클래스 정보 출력하기

--- src/test/java/core/ref/ReflectionTest.java
+++ src/test/java/core/ref/ReflectionTest.java
@@ -7,6 +7,10 @@ import org.slf4j.LoggerFactory;
 import next.model.Question;
 import next.model.User;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
 public class ReflectionTest {
     private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
 
@@ -14,6 +18,29 @@ public class ReflectionTest {
     public void showClass() {
         Class<Question> clazz = Question.class;
         logger.debug(clazz.getName());
+
+        Field[] fields = clazz.getDeclaredFields();
+        for (Field field : fields) {
+            logger.debug("field: {}", field.getName());
+        }
+
+        Constructor<?>[] constructors = clazz.getDeclaredConstructors();
+        for (Constructor<?> constructor : constructors) {
+            logger.debug("constructor: {}", constructor.getName());
+            Class<?>[] parameterTypes = constructor.getParameterTypes();
+            for (Class<?> parameterType : parameterTypes) {
+                logger.debug("parameter: {}", parameterType.getName());
+            }
+        }
+
+        Method[] methods = clazz.getDeclaredMethods();
+        for (Method method : methods) {
+            logger.debug("method: {}", method.getName());
+            Class<?>[] parameterTypes = method.getParameterTypes();
+            for (Class<?> parameterType : parameterTypes) {
+                logger.debug("parameter: {}", parameterType.getName());
+            }
+        }
     }
     
     @Test
