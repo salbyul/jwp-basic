e062732545057b8b0225750871dfb45f79eb4789
salbyul <kpast96@gmail.com>
2023-08-22 18:57:05 +0900

    test: @MyTest 어노테이션으로 설정된 메서드 실행하기

--- src/test/java/core/ref/Junit4TestRunner.java
+++ src/test/java/core/ref/Junit4TestRunner.java
@@ -2,10 +2,17 @@ package core.ref;
 
 import org.junit.Test;
 
+import java.lang.reflect.Method;
+
 public class Junit4TestRunner {
     @Test
     public void run() throws Exception {
         Class<Junit4Test> clazz = Junit4Test.class;
-
+        Method[] declaredMethods = clazz.getDeclaredMethods();
+        for (Method declaredMethod : declaredMethods) {
+            if (declaredMethod.isAnnotationPresent(MyTest.class)) {
+                declaredMethod.invoke(clazz.getConstructor().newInstance());
+            }
+        }
     }
 }
